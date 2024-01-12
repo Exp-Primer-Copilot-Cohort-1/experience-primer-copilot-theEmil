ddfcb98bfb172699a0ff899b51e1a87e8a3e43f9
theEmil <98152798+theEmil@users.noreply.github.com>
2024-01-12 19:44:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  const member = {
+    name: 'John Doe',
+    age: 30,
+    skills: ['HTML', 'CSS', 'JS'],
+  };
+  // Write code here
+  const { skills } = member;
+  return skills;
+}
